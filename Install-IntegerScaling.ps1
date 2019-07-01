62f2b9d6cc815e98a91b1a6a40326af739bb0397
Idearum <Idearum@users.noreply.github.com>
2019-07-01 23:38:55 +0200

    Added verification and errror handling
    Rewrote the script to allow for an easier base structure, while also adding various checks and error handling.

--- Install-IntegerScaling.ps1
+++ Install-IntegerScaling.ps1
@@ -1,61 +1,97 @@
-# PATHS
-$PathGameCompiledJS = '.\assets\js\game.compiled.js'
-$PathGUIenUS = '.\assets\data\lang\sc\gui.en_US.json'
+# Functions
+function Stop-Script {
+	[cmdletbinding()]
+	param([string]$Message = "")
 
-if((Test-Path -Path $PathGameCompiledJS) -eq $false -or (Test-Path -Path $PathGUIenUS) -eq $false)
-{
-	Write-Host "Could not find files. Be sure to run this script from the game install folder."
-	Read-Host
-	return;
+	Write-Warning $Message
+	Read-Host "Execution have terminated! Press Enter to close the window"
+	exit
 }
 
+Write-Host "Preparing patches..."
 
-# PATCHES - PathGameCompiledJS
+# Patches to apply
+$Patches = @(
+	[PSCustomObject]@{
+		File = '.\assets\js\game.compiled.js'
+		Changes = @(
+			[PSCustomObject]@{
+				Original = '{ORIGINAL:0,SCALE_X2:1,FIT:2,STRETCH:3};'
+				Patched = '{ORIGINAL:0,SCALE_X2:1,FIT:2,STRETCH:3,INTEGER:4};'
+			}
+			[PSCustomObject]@{
+				Original = 'sc.PIXEL_SIZE={ONE:0,TWO:1,THREE:2,FOUR:3};'
+				Patched = 'sc.PIXEL_SIZE={ONE:0,TWO:1,THREE:2,FOUR:3,FIVE:4,SIX:5};'
+			}
+			[PSCustomObject]@{
+				Original = 'else if(b=="pixel-size"){window.IG_GAME_SCALE=(this.values[b]||0)+1;localStorage.setItem("options.scale",window.IG_GAME_SCALE)}'
+				Patched = 'else if(b=="pixel-size"){window.IG_GAME_SCALE=(this.values[b]||0)+1;localStorage.setItem("options.scale",window.IG_GAME_SCALE);this._setDisplaySize();}'
+			}
+			[PSCustomObject]@{
+				Original = 'case sc.DISPLAY_TYPE.STRETCH:o=true;a=b;b=e;f=true;break;default:a=c;b=d'
+				Patched = 'case sc.DISPLAY_TYPE.STRETCH:o=true;a=b;b=e;f=true;break;case sc.DISPLAY_TYPE.INTEGER:f=true;if(b>c*window.IG_GAME_SCALE&&e>d*window.IG_GAME_SCALE){a=c*window.IG_GAME_SCALE;b=d*window.IG_GAME_SCALE}else if(Math.floor(b/c)==0||Math.floor(e/d)==0){a=c;b=d}else{if(b/c<e/d){a=c*Math.floor(b/c);b=d*Math.floor(b/c)}else{a=c*Math.floor(e/d);b=d*Math.floor(e/d)}}break;default:a=c;b=d'
+			}
+		)
+		Content = $null
+	}
+	[PSCustomObject]@{
+		File = '.\assets\data\lang\sc\gui.en_US.json'
+		Changes = @(
+			[PSCustomObject]@{
+				Original = '"display-type":{"name":"Display Type","group":["Original","Double","Fit","Stretch"]'
+				Patched = '"display-type":{"name":"Display Type","group":["Original","Double","Fit","Stretch","Integer"]'
+			},
+			[PSCustomObject]@{
+				Original = '"pixel-size":{"name":"Pixel Size","group":["1","2","3","4"]'
+				Patched = '"pixel-size":{"name":"Pixel Size","group":["1","2","3","4","5","6"]'
+			}
+		)
+		Content = $null
+	}
+)
 
-# sc.DISPLAY_TYPE - Add integer display type as an option.
-$OptionDisplayTypeSource 			= '{ORIGINAL:0,SCALE_X2:1,FIT:2,STRETCH:3};'
-$OptionDisplayTypeReplacement 		= '{ORIGINAL:0,SCALE_X2:1,FIT:2,STRETCH:3,INTEGER:4};'
+Write-Host "Verifying files..."
 
-# sc.PIXEL_SIZE - Add 5 and 6 as acceptable multipliers
-$OptionPixelSizeSource 				= 'sc.PIXEL_SIZE={ONE:0,TWO:1,THREE:2,FOUR:3};'
-$OptionPixelSizeReplacement 		= 'sc.PIXEL_SIZE={ONE:0,TWO:1,THREE:2,FOUR:3,FIVE:4,SIX:5};'
+# Pre-patch checks...
+ForEach ($Patch in $Patches)
+{
+	If((Test-Path -Path $Patch.File) -eq $false)
+	{
+		Stop-Script "One or more of the required files were not. Verify that the script is being run in the game folder."
+	} else {
+		
+		Write-Host "Reading file contents..."
+		$Patch.Content = Get-Content -Path $Patch.File -Raw
 
-# _checkSystemSettings -  Add a call to _setDisplaySize() (bandaid to properly apply the integer ratio scaling on launch).
-$_checkSystemSettingsSource 		= 'else if(b=="pixel-size"){window.IG_GAME_SCALE=(this.values[b]||0)+1;localStorage.setItem("options.scale",window.IG_GAME_SCALE)}'
-$_checkSystemSettingsReplacement 	= 'else if(b=="pixel-size"){window.IG_GAME_SCALE=(this.values[b]||0)+1;localStorage.setItem("options.scale",window.IG_GAME_SCALE);this._setDisplaySize();}'
+		if($Patch.Content)
+		{
+			ForEach ($Change in $Patch.Changes)
+			{
+				$MatchesFound = ($Patch.Content -split $Change.Original, 0, "simplematch" | Measure-Object | Select-Object -Exp Count) - 1
+				if($MatchesFound -ne 1)
+				{
+					Stop-Script "Expected 1 match but found $MatchesFound, in file '$($Patch.File)', for line:
+					$($Change.Original)"
+				}
+			}
+		} else {
+			Stop-Script "The file was empty."
+		}
+	}
+}
 
-# _setDisplaySize - Adds new case for handling integer base scaling.
-$_setDisplaySizeSource 				= 'case sc.DISPLAY_TYPE.STRETCH:o=true;a=b;b=e;f=true;break;default:a=c;b=d'
-$_setDisplaySizeReplacement 		= 'case sc.DISPLAY_TYPE.STRETCH:o=true;a=b;b=e;f=true;break;case sc.DISPLAY_TYPE.INTEGER:f=true;if(b>c*window.IG_GAME_SCALE&&e>d*window.IG_GAME_SCALE){a=c*window.IG_GAME_SCALE;b=d*window.IG_GAME_SCALE}else if(Math.floor(b/c)==0||Math.floor(e/d)==0){a=c;b=d}else{if(b/c<e/d){a=c*Math.floor(b/c);b=d*Math.floor(b/c)}else{a=c*Math.floor(e/d);b=d*Math.floor(e/d)}}break;default:a=c;b=d'
+# Everything looks fine, let's patch the files!
+ForEach ($Patch in $Patches)
+{
+	Write-Host "Applying patches to " $Patch.File "..."
 
-
-
-# PATCHES - PathGUIenUS
-
-# "display-type" - Add "integer" to the localization of the display type setting
-$DisplayTypeSource					= '"display-type":{"name":"Display Type","group":["Original","Double","Fit","Stretch"]'
-$DisplayTypeReplacement				= '"display-type":{"name":"Display Type","group":["Original","Double","Fit","Stretch","Integer"]'
-
-# "pixel-size" - Add 5 and 6 to the localization of the pixel size setting
-$PixelSizeSource 					= '"pixel-size":{"name":"Pixel Size","group":["1","2","3","4"]'
-$PixelSizeReplacement 				= '"pixel-size":{"name":"Pixel Size","group":["1","2","3","4","5","6"]'
-
-
-
-# OPERATION
-Write-Host "Patching '$PathGameCompiledJS'..."
-$FileGameCompiledJS = Get-Content -Path $PathGameCompiledJS -Raw
-$FileGameCompiledJS = $FileGameCompiledJS -replace [regex]::escape($OptionDisplayTypeSource), $OptionDisplayTypeReplacement
-$FileGameCompiledJS = $FileGameCompiledJS -replace [regex]::escape($OptionPixelSizeSource), $OptionPixelSizeReplacement
-$FileGameCompiledJS = $FileGameCompiledJS -replace [regex]::escape($_checkSystemSettingsSource), $_checkSystemSettingsReplacement
-$FileGameCompiledJS = $FileGameCompiledJS -replace [regex]::escape($_setDisplaySizeSource), $_setDisplaySizeReplacement
-$FileGameCompiledJS | Set-Content -Path $PathGameCompiledJS
-
-Write-Host "Patching '$PathGUIenUS'..."
-$FileGUIenUS = Get-Content -Path $PathGUIenUS -Raw
-$FileGUIenUS = $FileGUIenUS -replace [regex]::escape($DisplayTypeSource), $DisplayTypeReplacement
-$FileGUIenUS = $FileGUIenUS -replace [regex]::escape($PixelSizeSource), $PixelSizeReplacement
-$FileGUIenUS | Set-Content -Path $PathGUIenUS
+	ForEach ($Change in $Patch.Changes)
+	{
+		$Patch.Content = $Patch.Content -replace [regex]::escape($Change.Original), $Change.Patched
+	}
+	
+	$Patch.Content | Set-Content -Path $Patch.File
+}
 
 Write-Host "Patching finished. Press Enter to exit the script."
 Read-Host
