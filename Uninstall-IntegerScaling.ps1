96b65237d0245fa902731ccfd4c54636a725f6ab
Someone Somewhere <sackboydj167@gmail.com>
2021-03-07 19:48:07 +0000

    Update patches to work on version v1.4.0-1

--- Uninstall-IntegerScaling.ps1
+++ Uninstall-IntegerScaling.ps1
@@ -24,12 +24,12 @@ $Patches = @(
 				Patched = 'sc.PIXEL_SIZE={ONE:0,TWO:1,THREE:2,FOUR:3,FIVE:4,SIX:5};'
 			}
 			[PSCustomObject]@{
-				Original = 'else if(b=="pixel-size"){window.IG_GAME_SCALE=(this.values[b]||0)+1;localStorage.setItem("options.scale",window.IG_GAME_SCALE)}'
-				Patched = 'else if(b=="pixel-size"){window.IG_GAME_SCALE=(this.values[b]||0)+1;localStorage.setItem("options.scale",window.IG_GAME_SCALE);this._setDisplaySize();}'
+				Original = 'else if(a=="pixel-size"){window.IG_GAME_SCALE=(this.values[a]||0)+1;localStorage.setItem("options.scale",window.IG_GAME_SCALE)}'
+				Patched = 'else if(a=="pixel-size"){window.IG_GAME_SCALE=(this.values[a]||0)+1;localStorage.setItem("options.scale",window.IG_GAME_SCALE);this._setDisplaySize();}'
 			}
 			[PSCustomObject]@{
-				Original = 'case sc.DISPLAY_TYPE.STRETCH:o=true;a=b;b=e;f=true;break;default:a=c;b=d'
-				Patched = 'case sc.DISPLAY_TYPE.STRETCH:o=true;a=b;b=e;f=true;break;case sc.DISPLAY_TYPE.INTEGER:f=true;if(b>c*window.IG_GAME_SCALE&&e>d*window.IG_GAME_SCALE){a=c*window.IG_GAME_SCALE;b=d*window.IG_GAME_SCALE}else if(Math.floor(b/c)==0||Math.floor(e/d)==0){a=c;b=d}else{if(b/c<e/d){a=c*Math.floor(b/c);b=d*Math.floor(b/c)}else{a=c*Math.floor(e/d);b=d*Math.floor(e/d)}}break;default:a=c;b=d'
+				Original = 'case sc.DISPLAY_TYPE.STRETCH:k=true;a=b;b=i;j=true;break;default:a=c;b=d'
+				Patched = 'case sc.DISPLAY_TYPE.STRETCH:k=true;a=b;b=i;j=true;break;case sc.DISPLAY_TYPE.INTEGER:j=true;if(b>c*window.IG_GAME_SCALE&&i>d*window.IG_GAME_SCALE){a=c*window.IG_GAME_SCALE;b=d*window.IG_GAME_SCALE}else if(Math.floor(b/c)==0||Math.floor(i/d)==0){a=c;b=d}else{if(b/c<i/d){a=c*Math.floor(b/c);b=d*Math.floor(b/c)}else{a=c*Math.floor(i/d);b=d*Math.floor(i/d)}}break;default:a=c;b=d'
 			}
 		)
 		Content = $null
@@ -59,7 +59,6 @@ ForEach ($Patch in $Patches)
 	{
 		Stop-Script "One or more of the required files were not. Verify that the script is being run in the game folder."
 	} else {
-		
 		Write-Host "Reading file contents..."
 		$Patch.Content = Get-Content -Path $Patch.File -Raw
 
@@ -71,7 +70,7 @@ ForEach ($Patch in $Patches)
 				if($MatchesFound -ne 1)
 				{
 					Stop-Script "Expected 1 match but found $MatchesFound, in file '$($Patch.File)', for line:
-					$($Change.Patched)" 
+					$($Change.Patched)"
 				}
 			}
 		} else {
@@ -89,7 +88,7 @@ ForEach ($Patch in $Patches)
 	{
 		$Patch.Content = $Patch.Content -replace [regex]::escape($Change.Patched), $Change.Original
 	}
-	
+
 	$Patch.Content | Set-Content -Path $Patch.File
 }
 
