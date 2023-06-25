36532c1cd1f95705e44282f433da50ee3fd6bba4
Aemony <10578344+Aemony@users.noreply.github.com>
2023-06-25 20:14:35 +0200

    Added helper bats to bypass Execution Policy
    These batch files allows temporarily bypassing PowerShell's ExecutionPolicy configuration when executing the script

---
+++ Install-IntegerScaling.bat
@@ -0,0 +1,2 @@
+@CD /D "%~dp0"
+@powershell.exe -ExecutionPolicy Unrestricted -File  "%~dp0\Install-IntegerScaling.ps1"
---
+++ Uninstall-IntegerScaling.bat
@@ -0,0 +1,2 @@
+@CD /D "%~dp0"
+@powershell.exe -ExecutionPolicy Unrestricted -File  "%~dp0\Uninstall-IntegerScaling.ps1"
